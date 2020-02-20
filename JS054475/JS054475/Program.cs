da2eaf995919844190fdfb1c3dfcc5853af40045
Jakub Sałamun <zellanderplx@gmail.com>
2020-02-20 21:07:35 +0100

    123

---
+++ JS054475/JS054475/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JS054475
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Silnikinfi a = new Silnikinfi(190, 10);
+            Infinitii b = new Infinitii(a);
+            b.Jedz(10);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ JS054475/JS054475/Silnikinfi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JS054475
+{
+    public class Silnikinfi : Silnik
+    {
+        public Silnikinfi(int moc, double spalanie) : base(moc, spalanie, "brummm...")
+        {
+
+        }
+    }
+}
